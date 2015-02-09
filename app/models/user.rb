e332865871f46237758041193ab95d4c0a1bdccb
freedomevolves <samskinnerphd@gmail.com>
2015-02-09 06:18:41 -0500

    remove rolify from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  rolify
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
