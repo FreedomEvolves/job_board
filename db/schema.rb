3dbf765300277e276bffecc7ed05e0e1b546444a
freedomevolves <samskinnerphd@gmail.com>
2015-03-01 10:06:58 -0500

    add roles to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201162304) do
+ActiveRecord::Schema.define(version: 20150301143743) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150201162304) do
     t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
